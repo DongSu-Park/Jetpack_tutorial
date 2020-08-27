5f11e4aee9ad6d3ac19303d3b67413592090bae2
FloreErin <floreerin@gmail.com>
2020-08-27 14:32:11 +0900

    ViewModel ktx 적용

--- app/src/main/java/com/flore/room_kotlin_tutorial/MainActivity.kt
+++ app/src/main/java/com/flore/room_kotlin_tutorial/MainActivity.kt
@@ -4,27 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
+
+    // ViewModel ktx 사용
+    val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application!!)).get(MainViewModel::class.java)
-
-        viewModel.getAll().observe(this, Observer {todos ->
-            Log.d(TAG,"LiveData UI Update")
+        // LiveData 사용 (ViewModel 포함)
+        viewModel.getAll().observe(this, Observer { todos ->
+            Log.d(TAG, "LiveData UI Update")
             tv_result.text = todos.toString()
         })
 
+        // Coroutine 사용 (비동기 처리, ViewModel 포함)
         btn_add.setOnClickListener {
-            viewModel.insert(Todo(et_todo.text.toString()))
+            lifecycleScope.launch(Dispatchers.IO) {
+                viewModel.insert(Todo(et_todo.text.toString()))
+            }
         }
     }
 }
--- app/src/main/java/com/flore/room_kotlin_tutorial/MainViewModel.kt
+++ app/src/main/java/com/flore/room_kotlin_tutorial/MainViewModel.kt
@@ -6,16 +6,19 @@ import androidx.lifecycle.LiveData
 import androidx.room.Room
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+    // Room DB 생성
     private val db = Room.databaseBuilder(
-        application,
-        AppDatabase::class.java, "database-name"
-    ).allowMainThreadQueries().build()
+            application,
+    AppDatabase::class.java, "database-name"
+    ).build()
 
+    // LiveData 적용
     fun getAll(): LiveData<List<Todo>> {
         return db.todoDao().getAll()
     }
 
-    fun insert(todo : Todo){
+    // Coroutine 적용
+    suspend fun insert(todo : Todo){
         db.todoDao().insert(todo)
     }
 }
